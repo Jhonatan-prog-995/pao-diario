ed6505dd15b641f490d7a2b222c41187aae1cea5
Jhonatan-prog-995 <jhonatanrichard95@gmail.com>
2024-11-12 12:08:10 -0300

    inicio das postagem dos devocionais

---
+++ script.js
@@ -0,0 +1,23 @@
+const devocionalContainer = document.getElementById('devocional');
+
+
+fetch('dados.json')
+.then(response => response.json())
+.then(devocional => {
+
+    devocionalContainer.innerhtml = ' ';
+
+    devocional.forEach(devocional => {
+        devocionalContainer.innerHTML += `
+            <div class="devocional-item">
+                <h2>${devocional.titulo}</h2>
+                <p>${devocional.body}</p>
+                ${devocional.img ? `<img src="${devocional.img}" alt="Imagem Devocional">` : ''}
+            </div>    
+        
+        
+        `
+    })
+})
+
+.catch(error => console.error('Erro ao carregar o JSON:', error));
